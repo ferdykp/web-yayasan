ebc82e1c6ec493c652210af651d8d6b9d955c1b1
Mqdd <ahmadmiqdad27@gmail.com>
2026-09-22 00:49:39 +0700

    feat: cms page

--- app/Filament/Resources/NewsResource.php
+++ app/Filament/Resources/NewsResource.php
@@ -2,7 +2,7 @@
 namespace App\Filament\Resources;
 class NewsResource extends ContentResource {
  protected static ?string $model=\App\Models\News::class;
- protected static ?string $navigationLabel='Berita';
- protected static ?string $modelLabel='Berita';
+ protected static ?string $navigationLabel='Berita & Blog';
+ protected static ?string $modelLabel='Berita / Blog';
  public static function getPages():array {return ['index'=>NewsResource\Pages\ListRecords::route('/'),'create'=>NewsResource\Pages\CreateRecord::route('/create'),'edit'=>NewsResource\Pages\EditRecord::route('/{record}/edit')];}
 }
